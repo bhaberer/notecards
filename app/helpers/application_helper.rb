7c6ed87a767c3d1d18af002343703aae7078ad11
Brian Haberer <bhaberer@gmail.com>
2011-12-28 23:37:27 -0800

    First style pass, looking a little less like something to induce occular hemmorages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  
+  def entry_today?(user = current_user)
+    Card.where(:user_id => user, :day => Time.now.day, :month => Time.now.month, :year => Time.now.year).present?
+  end
+
+
 end
