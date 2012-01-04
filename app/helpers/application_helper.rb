7bdc3e3ca9cb6454d48cee018a9093afe1c87554
Brian Haberer <bhaberer@gmail.com>
2012-01-04 12:25:20 -0800

    Frontpage * Moving registration to partial for inclusion in frontpage * Added devise methods to app helper for devise forms on non devise pages. * Added some copy to homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,15 @@ module ApplicationHelper
     Card.where(:user_id => user, :day => Time.zone.now.day, :month => Time.zone.now.month, :year => Time.zone.now.year).present?
   end
 
+  def resource_name
+    :user
+  end
 
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
