614fe9b34c6380b18b871cc550e14fb2cead46cc
Brian Haberer <bhaberer@gmail.com>
2012-01-03 16:40:35 -0800

    Raise not found if viewing a profile that doesn't exist.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,53 +3,33 @@ class CardsController < ApplicationController
   before_filter :auth_check 
 
   def index
-    @cards = Card.all
-
+    @user = User.find_by_username(params[:username]) || raise("not found")
+    @cards = Card.where(:user => User.find_by_username(params[:username]))
+    
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @cards }
+      format.html 
     end
   end
 
   def month
-    @cards = Card.where(:month => params[:month], :user_id => User.find_by_username(params[:username]))
+    @cards = Card.where(:month => params[:month], 
+                        :user_id => User.find_by_username(params[:username]))
 
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @cards }
+      format.html
     end
   end
 
   def day
-    @cards = Card.where(:month => params[:month], :day => params[:day], :user_id => User.find_by_username(params[:username]))
+    @cards = Card.where(:month => params[:month], 
+                        :day => params[:day], 
+                        :user_id => User.find_by_username(params[:username]))
 
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @cards }
+      format.html 
     end
   end
 
-  def show
-    @card = Card.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @card }
-    end
-  end
-
-  def new
-    @card = Card.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @card }
-    end
-  end
-
-  def edit
-    @card = Card.find(params[:id])
-  end
 
   def create
     @card = Card.new(:entry => params[:card][:entry],
@@ -64,28 +44,6 @@ class CardsController < ApplicationController
       else
         format.html { render :action => "new" }
       end
-    end
-  end
-
-  def update
-    @card = Card.find(params[:id])
-
-    respond_to do |format|
-      if @card.update_attributes(params[:card])
-        format.html { redirect_to @card, :notice => 'Card was successfully updated.' }
-      else
-        format.html { render :action => "edit" }
-      end
-    end
-  end
-
-  def destroy
-    @card = Card.find(params[:id])
-    @card.destroy
-
-    respond_to do |format|
-      format.html { redirect_to cards_url }
-      format.json { head :ok }
     end
   end
 
