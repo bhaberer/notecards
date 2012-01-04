972ed7ee6fee5e7f80e5b08870af51142fd6c72d
Brian Haberer <bhaberer@gmail.com>
2012-01-03 21:46:35 -0800

    Fixes issue with the card written check not observing time_zone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def entry_today?(user = current_user)
-    Card.where(:user_id => user, :day => Time.now.day, :month => Time.now.month, :year => Time.now.year).present?
+    Card.where(:user_id => user, :day => Time.zone.now.day, :month => Time.zone.now.month, :year => Time.zone.now.year).present?
   end
 
 
