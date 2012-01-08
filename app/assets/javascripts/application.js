02bcac33719bd315b975111b74d4c56460726b4f
Brian Haberer <bhaberer@gmail.com>
2012-01-07 21:04:58 -0800

    Error messages and javascript work... blah blah

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require jquery_ujs
 //= require_tree .
   
-$(document).ready(function() {
+function deviseAjax() {
   $(document).find('.field input').each(function(index) {
     $(this).focus(function() {
       if ($(this).parent().attr('class') == 'field_with_errors') { 
@@ -23,9 +23,6 @@ $(document).ready(function() {
       }
     });
   });
-});
-
-$(document).ready(function() {
   $(document).find('.field label').each(function(index) {
     if ($('#' + $(this).attr('for')).val() != "") {
       $(this).hide();
@@ -35,13 +32,132 @@ $(document).ready(function() {
       $(this).hide();
     });
   });
-});
+};
 
-$(document).ready(function() {
-  $(document).find('#error_explanation li').each(function(index) {
+function moveErrors() {
+  $(document).find('.devise #error_explanation li').each(function(index) {
     $(this).css('top', $('#' + $(this).attr('for')).position().top);
     $(this).css('left', - $(this).width() - 15);
+    $(this).fadeIn();
   });
+  $(document).find('li[for="card_entry"]').each(function(index) {
+    $(this).css('top', $('#' + $(this).attr('for')).position().top);
+    $(this).css('left', $(this).position().left - 130);
+    $(this).fadeIn();
+  });
+};
+
+$(document).ready(function() {
+  moveErrors();
+  deviseAjax();
+});
+
+// Bunch of generic ajax helpers
+
+jQuery.ajaxSetup({ 
+  'beforeSend': function(xhr) {
+    xhr.setRequestHeader("Accept", "text/javascript");
+  };
+});
+
+function _ajax_request(url, data, callback, type, method) {
+  if (jQuery.isFunction(data)) {
+      callback = data;
+      data = {};
+  }
+  return jQuery.ajax({
+    type: method,
+    url: url,
+    data: data,
+    success: callback,
+    dataType: type
+  });
+};
+
+jQuery.extend({
+  put: function(url, data, callback, type) {
+    return _ajax_request(url, data, callback, type, 'PUT');
+  },
+  delete_: function(url, data, callback, type) {
+    return _ajax_request(url, data, callback, type, 'DELETE');
+  }
+});
+
+jQuery.fn.submitWithAjax = function() {
+  this.unbind('submit', false);
+  this.submit(function() {
+    $.post(this.action, $(this).serialize(), null, "script");
+    $("#form_spinner").fadeIn();
+    return false;
+  })
+  return this;
+};
+
+jQuery.fn.searchWithAjax = function() {
+  this.unbind('submit', false);
+  this.submit(function() {
+    $.get(this.action, $(this).serialize(), null, "script");
+    return false;
+  })
+  return this;
+};
+
+jQuery.fn.getWithAjax = function() {
+  this.unbind('click', false);
+  this.click(function() {
+    $.get($(this).attr("href"), $(this).serialize(), null, "script");
+    return false;
+  })
+  return this;
+};
+
+jQuery.fn.postWithAjax = function() {
+  this.unbind('click', false);
+  this.click(function() {
+    $.post($(this).attr("href"), $(this).serialize(), null, "script");
+    return false;
+  })
+  return this;
+};
+
+jQuery.fn.putWithAjax = function() {
+  this.unbind('click', false);
+  this.click(function() {
+    $.put($(this).attr("href"), $(this).serialize(), null, "script");
+    return false;
+  })
+  return this;
+};
+
+jQuery.fn.deleteWithAjax = function() {
+  this.removeAttr('onclick');
+  this.unbind('click', false);
+  this.click(function() {
+    $.delete_($(this).attr("href"), $(this).serialize(), null, "script");
+    return false;
+  })
+  return this;
+};
+
+function ajaxLinks() {
+  $('.ajaxForm').submitWithAjax();
+  $('.searchForm').searchWithAjax();
+  $('a.get').getWithAjax();
+  $('a.post').postWithAjax();
+  $('a.put').putWithAjax();
+  $('a.delete').deleteWithAjax();
+}
+
+$(document).ready(function() {
+  // All non-GET requests will add the authenticity token
+  $(document).ajaxSend(function(event, request, settings) {
+    if (typeof(window.AUTH_TOKEN) == "undefined") return;
+    // IE6 fix for http://dev.jquery.com/ticket/3155
+    if (settings.type == 'GET' || settings.type == 'get') return;
+    settings.data = settings.data || "";
+    settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(window.AUTH_TOKEN);
+  });
+  ajaxLinks();
 });
 
 
