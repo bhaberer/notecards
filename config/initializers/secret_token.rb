4027f5c3f1c8a2c513e22d1c3e10b74ab6b0b451
Brian Haberer <bhaberer@gmail.com>
2013-06-12 02:05:40 -0700

    hey lets not use the secret token in git on production huh?

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Notecards::Application.config.secret_token = 'a6508fabefc770a0d77751eef33f03af9e4bee651c7cbf7484f16ec92e6996d5b72c537eb21000d2f790b25f91ffbc9ff4383077877f4e3ffdfc65175b692ff2'
+Notecards::Application.config.secret_token = ENV['secret_token'] || 'a6508fabefc770a0d77751eef33f03af9e4bee651c7cbf7484f16ec92e6996d5b72c537eb21000d2f790b25f91ffbc9ff4383077877f4e3ffdfc65175b692ff2'
